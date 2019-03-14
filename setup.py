10c6fc924823baa19ff0c86a021d43d4d6eb25a9
Nikshep Saravanan <nikshepsvn@Niksheps-MBP.hsd1.ca.comcast.net>
2019-03-13 20:05:43 -0700

    fixed errors that caused pyttsx3 to fail building

--- setup.py
+++ setup.py
@@ -1,28 +1,6 @@
 import platform
 from setuptools import setup
 
-
-extras_require = {
-    ':"darwin" in sys_platform': [
-        'pyobjc>=2.4',
-    ],
-    ':"win32" in sys_platform': [
-        'pypiwin32',
-    ],
-}
-
-# Ubuntu: sudo apt install espeak
-install_requires = []
-if platform.system() == 'Windows':
-    install_requires += [
-        'pypiwin32'
-    ]
-elif platform.system() == 'Darwin':
-    install_requires += [
-        'pyobjc>=2.4'
-    ]
-
-
 with open('README.rst', 'r') as f:
     long_description = f.read()
 
@@ -38,8 +16,6 @@ setup(
     url='https://github.com/nateshmbhat/pyttsx3',
     author_email='nateshmbhatofficial@gmail.com',
     #download_url = 'https://github.com/nateshmbhat/pyttsx3/archive/v2.6.tar.gz',
-    install_requires=install_requires
     keywords=['pyttsx' , 'ivona','pyttsx for python3' , 'TTS for python3' , 'pyttsx3' ,'text to speech for python','tts','text to speech','speech','speech synthesis','offline text to speech','offline tts','gtts'],
-    classifiers = [] ,
-    extras_require=extras_require
+    classifiers = []
 )
